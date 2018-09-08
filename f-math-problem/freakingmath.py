1c821a0d495a29bcb8e4970eb960a5d2019e22e1
Tuan Anh <anht.hoang158@gmail.com>
2018-09-08 16:33:30 +0700

    Lab3 game

--- f-math-problem/freakingmath.py
+++ f-math-problem/freakingmath.py
@@ -1,8 +1,27 @@
-from random import *
+from random import randint, choice
+from calc_function import calculate
+
 
 def generate_quiz():
     # Hint: Return [x, y, op, result]
-    return [0, 0, '@@', 12]
+    x = randint(0, 9)
+    y = randint(0, 9)
+    op = choice(["+", "-", "*", "/"])
+    error = randint(-1, 1)
+    result = calculate(x, y, op) + error
+
+    return [x, y, op, result]
+
 
 def check_answer(x, y, op, result, user_choice):
-    pass
+    if result == calculate(x, y, op):
+        if user_choice is True:
+            return True
+        else:
+            return False
+
+    else:
+        if user_choice is True:
+            return False
+        else:
+            return True
